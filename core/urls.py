f0fd6340c749dc1cf16f53ec63cc69ea4c7fde50
illa-j <iliacdmn@gmail.com>
2026-01-07 18:11:10 +0200

    feat: create routes for views

---
+++ core/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from core.views import (
+    IndexView,
+    TagListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    toggle_update_is_done,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path(
+        "tasks/<int:pk>/toggle-is-done/",
+        toggle_update_is_done,
+        name="task-toggle-is-done",
+    ),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+]
+
+app_name = "core"
